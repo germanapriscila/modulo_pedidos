1f4afadc713284aeafb2c01d82c429b0539f9a30
Germana Priscila <priscilafortaleza@gmail.com>
2024-11-21 20:11:29 -0300

    Correção na estrutura de pastas

--- html/index.php
+++ html/index.php
@@ -1,3 +1,25 @@
 <?php
+  require_once "views/header.php";
+ ?>
 
-echo "Hello World";
+<main>
+    <div id="title-container">
+      <h1>Teste</h1>
+    </div>
+    <!-- <div id="posts-container">
+      <?php foreach($posts as $post): ?>
+        <div class="post-box">
+          <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+          <h2 class="post-title">
+            <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
+          </h2>
+          <p class="post-description"><?= $post['description'] ?></p>
+          <div class="tags-container">
+            <?php foreach($post['tags'] as $tag): ?>
+              <a href="#"><?= $tag ?></a>
+            <?php endforeach; ?>
+          </div>
+        </div>
+      <?php endforeach; ?>
+    </div> -->
+  </main>
